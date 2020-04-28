57f4c91dbc119c638a6b6349621788c0ed0ed837
Tanakrid F <tanakrid.0011@gmail.com>
2020-04-28 10:36:56 +0700

    [DRIVER] driver thing

--- CSC209/[SELF]OperationConverter/src/Driver.java
+++ CSC209/[SELF]OperationConverter/src/Driver.java
@@ -1,18 +1,21 @@
 public class Driver {
     public static void main(String[] args) {
-        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2")));
-        System.out.println(OperationConverter.infixToPostfix("4 + (6*5/3) - 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + (6*5/3) - 2")));
-        System.out.println(OperationConverter.infixToPostfix("(4 + 6) * 5/3 - 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("(4 + 6) * 5/3 - 2")));
-        System.out.println(OperationConverter.infixToPostfix("4 * 6 * 5 + 3 - 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 * 6 * 5 + 3 - 2")));
-        System.out.println(OperationConverter.infixToPostfix("4 * 6 + 5 *  3 ^ 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 * 6 + 5 *  3 ^ 2")));
-        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 ^ 3 - 2"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + 6 * 5 ^ 3 - 2")));
-        System.out.println(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3"));
-        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3")));
+//        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2")));
+//        System.out.println(OperationConverter.infixToPostfix("4 + (6*5/3) - 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + (6*5/3) - 2")));
+//        System.out.println(OperationConverter.infixToPostfix("(4 + 6) * 5/3 - 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("(4 + 6) * 5/3 - 2")));
+//        System.out.println(OperationConverter.infixToPostfix("4 * 6 * 5 + 3 - 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 * 6 * 5 + 3 - 2")));
+//        System.out.println(OperationConverter.infixToPostfix("4 * 6 + 5 *  3 ^ 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 * 6 + 5 *  3 ^ 2")));
+//        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 ^ 3 - 2"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("4 + 6 * 5 ^ 3 - 2")));
+//        System.out.println(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3"));
+//        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3")));
+        //Part 2
+//        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2"));
+        System.out.println(OperationConverter.calculatePostfix("4  6  5  3  ^  *  +  2  -"));
     }
 }
