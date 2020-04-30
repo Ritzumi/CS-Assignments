dfa7a7ba26b040516531a92adf32c22facfb1f96
Tanakrid F <tanakrid.0011@gmail.com>
2020-04-30 18:06:31 +0700

    [QUIZ] check my answer

--- CSC209/[SELF]OperationConverter/src/Driver.java
+++ CSC209/[SELF]OperationConverter/src/Driver.java
@@ -15,7 +15,7 @@ public class Driver {
 //        System.out.println(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3"));
 //        System.out.println(OperationConverter.calculatePostfix(OperationConverter.infixToPostfix("(4 + (6 + 5 * 7 ^ 8 + 2)) * 9 + 3")));
         //Part 2
-//        System.out.println(OperationConverter.infixToPostfix("4 + 6 * 5 / 3 - 2"));
-        System.out.println(OperationConverter.calculatePostfix("4  6  5  3  ^  *  +  2  -"));
+        System.out.println(OperationConverter.infixToPostfix("6 * ( ( 9 + 7 / 4 * 8) * 5 ) + 3"));
+        System.out.println(OperationConverter.calculatePostfix("0  5   8  *   4   /  6   3   +   2   -  *  +"));
     }
 }
