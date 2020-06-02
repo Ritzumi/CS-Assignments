67fd97686c8ee26af8069478c3257a558e9e7e0c
Tanakrid F <tanakrid.0011@gmail.com>
2020-06-02 16:14:02 +0700

    [ADD] Vue practice lesson 10

--- Personal/Vue-Practice/app.js
+++ Personal/Vue-Practice/app.js
@@ -2,9 +2,8 @@ new Vue({
     el: '#vue-app',
 
     data:{
-        a: 0,
-        b: 0,
-        age: 20
+        available: false,
+        nearby: false
     },
 
     methods:{
@@ -12,14 +11,12 @@ new Vue({
     },
 
     computed:{
-        addToA: function(){
-            console.log('add to A')
-            return this.a + this.age
-        },
-        addToB: function(){
-            console.log('add to B')
-            return this.b + this.age
-        },
+        compClasses: function(){
+            return {
+                available: this.available,
+                nearby: this.nearby
+            }
+        }
     }
 
 });
