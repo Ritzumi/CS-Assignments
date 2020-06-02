46ae32c067f05a0c1942c1912c7da01bfbeaaeee
Tanakrid F <tanakrid.0011@gmail.com>
2020-06-02 15:50:26 +0700

    [ADD] Vue practice lesson 7-8

--- Personal/Vue-Practice/app.js
+++ Personal/Vue-Practice/app.js
@@ -2,32 +2,16 @@ new Vue({
     el: '#vue-app',
 
     data:{
-        name: "Meow",
-        website: "https://www.facebook.com/",
-        tag: "<a href=\"https://www.facebook.com/\">Facebook Link</a>",
-        year: 2020,
-        x: 0,
-        y: 0
+        name: '',
+        age: ''
     },
 
     methods:{
-        greet: function(params){
-            if(params == undefined)
-                params = "";
-            return "Hello there " + params + " " + this.name
+        logName: function(){
+            console.log('Entered Name')
         },
-        add: function(num){
-            this.year+= num;
-        },
-        substract: function(num){
-            this.year-= num;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('Hi');
+        logAge: function(){
+            console.log('Entered Age')
         }
     }
 });
